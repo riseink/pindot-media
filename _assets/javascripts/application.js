7513c3b7de51bee1bc68b3ac601d6df6dd2c0b79
Digital Traffic <digitaltraffic@digitraf2.local>
2017-02-21 13:54:40 -0600

    jeet jekyll foundation

---
+++ _assets/javascripts/application.js
@@ -0,0 +1,35 @@
+//= require vendor/jquery
+//= require vendor/modernizr-2.8.3.min
+
+var app = {
+
+    initialize: function(){
+        this.initFunction();
+        
+    },
+    
+
+
+    initFunction: function () {
+
+		//do something
+    }
+
+
+    
+};
+
+$(document).ready(function(){
+    app.initialize();
+
+    $(window).on("resize", function(){
+		// RESIZE
+      
+    });
+	
+	$(window).load(function() {
+		// Race Conditions
+	});
+
+});
+
